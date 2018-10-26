97cef6ab773ca00e9bc06cee9ffe887008f2b9ab
Paresh Borkar <paresh@threatwatch.io>
2018-10-26 04:33:13 -0700

    Fix indentation for documentation in code.

--- pytw/exceptions.py
+++ pytw/exceptions.py
@@ -5,12 +5,12 @@ class pytw_error(Exception):
         :param msg: Message for the exception raised
     """
 
-	def __init__(self, msg):
-		self.msg = msg
+    def __init__(self, msg):
+        self.msg = msg
 
-	def __repr__(self):
-		return "<pytw_error: %s>" % self.msg
+    def __repr__(self):
+        return "<pytw_error: %s>" % self.msg
 
-	def __str__(self):
-		return str(self.msg)
+    def __str__(self):
+        return str(self.msg)
 
